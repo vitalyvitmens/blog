375622a7b317fcc061294a849ceffb3cfed4a873
vitalyvitmens <otm_@tut.by>
2023-09-06 17:08:07 +0300

    Редактирование статьи. Подготовка редактора

--- my-app/src/pages/post/components/post-content/post-content.js
+++ my-app/src/pages/post/components/post-content/post-content.js
@@ -1,34 +1,27 @@
 import { H2, Icon } from '../../../../components'
+import { SpecialPanel } from '../special-panel/special-panel'
 import { styled } from 'styled-components'
 
 const PostContentContainer = ({
 	className,
-	post: { id, title, imageUrl, content, publishedAt },
+	post: { title, imageUrl, content, publishedAt },
 }) => {
 	return (
 		<div className={className}>
 			<img src={imageUrl} alt={title} />
 			<H2>{title}</H2>
-			<div className="special-panel">
-				<div className="published-at">
-					<Icon
-						id="fa-calendar-o"
-						size="18px"
-						margin="0 7px 0 0"
-						onClick={() => {}}
-					/>
-					{publishedAt}
-				</div>
-				<div className="buttons">
+			<SpecialPanel
+				publishedAt={publishedAt}
+				margin="-20px 0 20px"
+				editButton={
 					<Icon
 						id="fa-pencil-square-o"
 						size="21px"
 						margin="0 10px 0 0"
 						onClick={() => {}}
 					/>
-					<Icon id="fa-trash-o" size="21px" onClick={() => {}} />
-				</div>
-			</div>
+				}
+			/>
 			<div className="post-text">{content}</div>
 		</div>
 	)
@@ -40,28 +33,8 @@ export const PostContent = styled(PostContentContainer)`
 		margin: 0 20px 10px 0;
 	}
 
-	& .special-panel {
-		display: flex;
-		justify-content: space-between;
-		margin: -20px 0 20px;
-	}
-
-	& .published-at {
-		display: flex;
-		align-items: center;
-		font-size: 18px;
-	}
-
-	& i {
-		position: relative;
-		top: -1px;
-	}
-
-	& .buttons {
-		display: flex;
-	}
-
 	& .post-text {
 		font-size: 18px;
+		white-space: pre-line;
 	}
 `
