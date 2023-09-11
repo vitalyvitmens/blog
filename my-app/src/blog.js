0fb9494b838bd12977e091bb6859ad821571aac8
vitalyvitmens <otm_@tut.by>
2023-09-11 13:22:24 +0300

    Получение статей

--- my-app/src/blog.js
+++ my-app/src/blog.js
@@ -2,7 +2,7 @@ import { useLayoutEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { Header, Footer, Modal } from './components'
-import { Authorization, Post, Registration, Users } from './pages'
+import { Authorization, Main, Post, Registration, Users } from './pages'
 import { setUser } from './actions'
 import styled from 'styled-components'
 
@@ -13,7 +13,7 @@ const AppColumn = styled.div`
 	width: 1000px;
 	min-height: 100%;
 	margin: 0 auto;
-  background-color: antiquewhite;
+	background-color: antiquewhite;
 `
 
 const Page = styled.div`
@@ -45,7 +45,7 @@ export const Blog = () => {
 			<Header />
 			<Page>
 				<Routes>
-					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/" element={<Main />} />
 					<Route path="/login" element={<Authorization />} />
 					<Route path="/register" element={<Registration />} />
 					<Route path="/users" element={<Users />} />
