fa8ec7ab51655b127dec475fe52c672caad88385
vitalyvitmens <otm_@tut.by>
2023-09-12 13:29:30 +0300

    Реализовать поиск на Main

--- my-app/src/bff/api/get-posts.js
+++ my-app/src/bff/api/get-posts.js
@@ -1,13 +1,13 @@
 import { transformPost } from '../transformers'
 
-export const getPosts = (page, limit) =>
-	fetch(`http://localhost:3005/posts?_page=${page}&_limit=${limit}`)
+export const getPosts = (searchPhrase, page, limit) =>
+	fetch(
+		`http://localhost:3005/posts?title_like=${searchPhrase}&_page=${page}&_limit=${limit}`
+	)
 		.then((loadedPosts) =>
 			Promise.all([loadedPosts.json(), loadedPosts.headers.get('Link')])
 		)
-		.then(
-			([loadedPosts, links]) => ({
-        posts: loadedPosts && loadedPosts.map(transformPost),
-        links,
-      })
-		)
+		.then(([loadedPosts, links]) => ({
+			posts: loadedPosts && loadedPosts.map(transformPost),
+			links,
+		}))
